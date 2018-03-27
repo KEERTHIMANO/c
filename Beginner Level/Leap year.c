a98a391c1853de14d5a88ac9f4903847ad20c5ad
KEERTHIMANO <37831362+KEERTHIMANO@users.noreply.github.com>
2018-03-27 15:47:04 +0530

    Update Leap year.c

--- Beginner Level/Leap year.c
+++ Beginner Level/Leap year.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-
+ 
 int main(void) {
-	int n;
-	if (n%4 == 0)
+int n;
+if(n==0)
+printf("\n The Year is Not Valid");
+else	if (n%4 == 0)
 	printf("The given year is Leap year");
 	else 
 	printf("The given year is Not Leap Year");
+ 
 	return 0;
 }
+ 
